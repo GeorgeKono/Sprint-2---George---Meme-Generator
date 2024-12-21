99d45527b809289cdaf858d79219e39a30cdf3fc
George Konovalov <george.konovalov@gmail.com>
2024-12-21 19:28:59 +0200

    Add renderGallery (with two images)

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -1,1 +1,15 @@
 'use strict'
+
+function onInit() {
+    renderGallery()
+}
+
+function renderGallery() {
+    const elMemesGallery = document.querySelector('.memes-grid-container')
+
+    const strHtmls = gImgs.map(gImgs => 
+        `<img src="${gImgs.url}" onclick=onMemePicked(this)></img>`
+    )
+
+    elMemesGallery.innerHTML = strHtmls.join('')
+}
