e6f79e2b99a6db910c4037f5ba4503d0ece7682b
George Konovalov <george.konovalov@gmail.com>
2024-12-21 18:41:09 +0200

    Add getMeme() function and gMeme effect on editor

--- js/editor.controller.js
+++ js/editor.controller.js
@@ -3,6 +3,8 @@
 let gElCanvas
 let gCtx
 
+const meme = getMeme()
+
 renderMeme()
 
 function renderMeme() {
@@ -16,23 +18,28 @@ function renderMeme() {
 
 function drawImg() {
     const elImg = new Image()
-    elImg.src = 'img/1.jpg'
+    elImg.src = `img/${meme.selectedImgId}.jpg`
     
     elImg.onload = () => {
         gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
-        onAddTextLine( { offsetX: gElCanvas.width / 2, offsetY: gElCanvas.height / 5 } )
+        onAddTextLine( 
+            { offsetX: gElCanvas.width / 2, offsetY: gElCanvas.height / 5 },
+            meme.lines[0].txt,
+            meme.lines[0].size,
+            meme.lines[0].color
+         )
     }
 }
 
-function onAddTextLine (ev) {
+function onAddTextLine (ev, txt = 'Add Text Here', size, color) {
     const { offsetX, offsetY } = ev
     gCtx.lineWidth = 2
     gCtx.strokeStyle = 'black'
-    gCtx.fillStyle = 'white'
-    gCtx.font = 'bold 70px impact'
+    gCtx.fillStyle = color
+    gCtx.font = `bold ${size}px impact`
     gCtx.textAlign = 'center'
     gCtx.textBaseline = 'middle'
 
-    gCtx.fillText('Add Text Here', offsetX, offsetY)
-    gCtx.strokeText('Add Text Here', offsetX, offsetY)
+    gCtx.fillText(txt, offsetX, offsetY)
+    gCtx.strokeText(txt, offsetX, offsetY)
 }
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,18 +1,24 @@
 'use strict'
 
-var gImgs = [{id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat']}]
+var gImgs = [
+    {id: 1, url: 'img/1.jpg', keywords: ['angry', 'political', 'trump']},
+    {id: 2, url: 'img/2.jpg', keywords: ['lovely', 'puppies', 'dogs']},
+]
 
 var gMeme = {
-    selectedImgId: 5,
+    selectedImgId: 1,
     selectedLineIdx: 0,
     lines: [
         {
             txt: 'I sometimes eat Falafel',
-            size: 20,
-            color: 'red',
+            size: 50,
+            color: 'white',
         }
     ]
 }
 
 var gKeywordSearchCountMap = {'funny': 12, 'cat': 16, 'baby': 2,}
 
+function getMeme() {
+    return gMeme
+}
