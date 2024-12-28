c0168194dd69e85fc1c574c105979a3340ae6605
George Konovalov <george.konovalov@gmail.com>
2024-12-28 19:43:18 +0200

    Add more images

--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -3,6 +3,22 @@
 var gImgs = [
     {id: 1, url: 'img/1.jpg', keywords: ['angry', 'political', 'trump']},
     {id: 2, url: 'img/2.jpg', keywords: ['lovely', 'puppies', 'dogs']},
+    {id: 3, url: 'img/3.jpg', keywords: ['lovely', 'puppies', 'babies']},
+    {id: 4, url: 'img/4.jpg', keywords: ['lovely', 'cat', 'dreaming']},
+    {id: 5, url: 'img/5.jpg', keywords: ['strong', 'winner', 'babies']},
+    {id: 6, url: 'img/6.jpg', keywords: ['thinking', 'history', 'man']},
+    {id: 7, url: 'img/7.jpg', keywords: ['suprising', 'big eyes', 'babies']},
+    {id: 8, url: 'img/8.jpg', keywords: ['smiling', 'clown', 'man']},
+    {id: 9, url: 'img/9.jpg', keywords: ['smiling', '', 'babies']},
+    {id: 10, url: 'img/10.jpg', keywords: ['smiling', 'political', 'obama']},
+    {id: 11, url: 'img/11.jpg', keywords: ['kissing', 'boxing', 'fight']},
+    {id: 12, url: 'img/12.jpg', keywords: ['thinking', 'busted', 'israeli']},
+    {id: 13, url: 'img/13.jpg', keywords: ['smiling', 'move', 'dicaprio']},
+    {id: 14, url: 'img/14.jpg', keywords: ['movie', 'matrix', 'glases']},
+    {id: 15, url: 'img/15.jpg', keywords: ['movie', 'lord of the rings', 'thinkins']},
+    {id: 16, url: 'img/16.jpg', keywords: ['movie', 'smiling', 'star trek']},
+    {id: 17, url: 'img/17.jpg', keywords: ['thinking', 'political', 'putin']},
+    {id: 18, url: 'img/18.jpg', keywords: ['movie', 'smiling', 'buzz']},
 ]
 
 var gMeme = {
